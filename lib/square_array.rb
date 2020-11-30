7f03cf37a1cfcb3685183c5ed9149183dfc5cc34
Reed Roffis <reedroffis18@gmail.com>
2020-11-30 05:00:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0 
   new_array = []
   while count < array.length do
-  new_array.push(array) ** 2 
+  new_array.push(array[count] ** 2) 
   end
   square_array(array)
 end
