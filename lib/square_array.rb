9f43c7bc7e888d05b67d0c0ecf6b5b377908c756
Reed Roffis <reedroffis18@gmail.com>
2020-11-30 04:47:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,5 +3,8 @@ numbers = [1, 2, 3]
 def square_array(numbers)
   count = 0 
   while count < numbers.length do
-    numbers.index ** 2
+    numbers.index ** 2 
+  end
+  square_array(numbers)
+  binding.pry 
 end
